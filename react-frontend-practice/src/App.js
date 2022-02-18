53e60c59b8d81aad204cce7fa08dcfa70f867105
Viper-10 <priyadharshanbeepee@gmail.com>
2022-02-18 19:46:56 +0530

    Initial Commit

---
+++ react-frontend-practice/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;

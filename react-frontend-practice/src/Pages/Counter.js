67206e31f0b640e7e4c9871e89486bafb2de5952
Viper-10 <priyadharshanbeepee@gmail.com>
2022-03-04 22:21:04 +0530

    Add - ComponentDidMount, ComponentDidUpdate equivalent in Hooks
    Modify - Counter style

--- react-frontend-practice/src/Pages/Counter.js
+++ react-frontend-practice/src/Pages/Counter.js
@@ -3,57 +3,90 @@ import axios from "axios";
 import ColorDataView from "../Components/ColorDataView";
 
 const Counter = () => {
-  const [counter, setCounter] = useState(0);
-  const [colorData, setColorData] = useState([]);
+  const [count, setCount] = useState(0);
+  const [toggle, setToggle] = useState(false);
+  const [colors, setColors] = useState([]);
+
+  const buttonStyle = {
+    padding: "0.5rem 1rem",
+    fontSize: "1.5rem",
+    cursor: "pointer",
+    backgroundColor: "#ddd",
+    width: "max-content",
+    margin: "1rem auto",
+  };
+
+  // ComponentDidUpdate
+  // Called after every render
+  useEffect(() => {
+    console.log("Component Rendered Everytime any state changes");
+  });
+
+  // ComponentDidMount
+  // called only once
+  useEffect(() => {
+    console.log("Component Rendered Only Once");
+  }, []);
 
   useEffect(() => {
-    if (counter === 0) {
-      setColorData([]);
-    } else {
-      axios
-        .get(
-          `https://random-data-api.com/api/color/random_color?size=${counter}`
-        )
-        .then(({ data }) => {
-          setColorData(colorData.concat(data));
-        })
-        .catch((error) => console.log(error));
-    }
-  }, [counter]);
+    setToggle(!toggle);
+    // axios api call
+    axios
+      .get(`https://random-data-api.com/api/color/random_color?size=${count}`)
+      .then((response) => {
+        const { data } = response;
+        setColors(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [count]);
+  const increment = () => {
+    setCount(() => count + 1);
+  };
 
+  const decrement = () => {
+    setCount(() => count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
   return (
-    <div className="counter">
-      <h1>Counter and Api Calls</h1>
-      <div>
-        <p>{counter}</p>
-        <button
-          onClick={() => {
-            setCounter(counter + 1);
-          }}
-        >
-          Increment
-        </button>
-        <button
-          onClick={() => {
-            setCounter(0);
-          }}
-        >
-          Reset
-        </button>
+    <div style={{ textAlign: "center", marginTop: "1rem" }}>
+      <button style={buttonStyle} onClick={increment}>
+        +
+      </button>
+      <span style={buttonStyle}>{count}</span>
+      <button style={buttonStyle} onClick={decrement}>
+        -
+      </button>
+
+      <div style={buttonStyle} onClick={reset}>
+        Reset
       </div>
 
-      {colorData.map((data) => (
+      {toggle && <h2>You can see me!!! Toggle counter to hide me</h2>}
+
+      {colors.map((color) => (
         <ColorDataView
-          key={data.id}
-          color_name={data.color_name}
-          hex_value={data.hex_value}
+          key={color.id}
+          color_name={color.color_name}
+          hex_value={color.hex_value}
         />
       ))}
     </div>
   );
 };
 
-{
-  /* <ColorDataView colorData={data} /> */
-}
 export default Counter;
+
+{
+  /* {colorData.map((data) => (
+        <ColorDataView
+          key={data.id}
+          color_name={data.color_name}
+          hex_value={data.hex_value}
+        />
+      ))} */
+}
